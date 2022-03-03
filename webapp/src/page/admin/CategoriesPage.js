fbc70d38f4bda20a2f54273a3b9a4800e0f7c43f
Lirjeta <lirjeta@Lirjetas-MacBook-Pro.local>
2022-03-03 10:24:50 -0500

    Merge with HK branch

--- webapp/src/page/admin/CategoriesPage.js
+++ webapp/src/page/admin/CategoriesPage.js
@@ -1,6 +1,26 @@
+import CustomMaterialTable from "../../component/CustomMaterialTable";
+import {QueryKeys} from "../../service/QueryKeys";
+import {useRef} from "react";
+import {TextFieldTableCell} from "../../component/TableCells";
+import {CategoryService} from "../../service/CategoryService";
+
+const categoriesService = new CategoryService();
+
 export default function CategoriesPage({}) {
 
+    const errorRef = useRef();
+
+    const columns = [
+        {title: 'Name', field: 'name', editComponent: props => TextFieldTableCell(props, errorRef)},
+    ];
+
     return (
-        <></>
+        <CustomMaterialTable
+            title="Manage Categories"
+            columns={columns}
+            service={categoriesService}
+            queryKey={QueryKeys.CATEGORIES}
+            errorRef={errorRef}
+        />
     );
-}
+} 
--- webapp/src/page/admin/ServicesAdminPage.js
+++ webapp/src/page/admin/ServicesAdminPage.js
@@ -1,6 +1,29 @@
+import CustomMaterialTable from "../../component/CustomMaterialTable";
+import {QueryKeys} from "../../service/QueryKeys";
+import {useRef} from "react";
+import {TextFieldTableCell} from "../../component/TableCells";
+import ServiceService from "../../service/ServiceService";
+
+const serviceService = new ServiceService();
+
 export default function ServicesAdminPage({}) {
 
+    const errorRef = useRef();
+
+    const columns = [
+        {title: 'Name', field: 'name', editComponent: props => TextFieldTableCell(props, errorRef)},
+        {title: 'Duration (Minutes)', field: 'durationInMinutes', editComponent: props => TextFieldTableCell(props, errorRef), initialEditValue: 0},
+        {title: 'Price', field: 'price', editComponent: props => TextFieldTableCell(props, errorRef), initialEditValue: 0.00},
+        {title: 'Enabled', type: 'boolean', field: 'enabled', initialEditValue: true}
+    ];
+
     return (
-        <></>
+        <CustomMaterialTable
+            title="Manage Services"
+            columns={columns}
+            service={serviceService}
+            queryKey={QueryKeys.SERVICES}
+            errorRef={errorRef}
+        />
     );
-}
+} 
