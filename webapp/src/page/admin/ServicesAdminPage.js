950b603dcd15be8f6707b708a1d617951b7df4b1
Lirjeta <lirjeta@Lirjetas-MacBook-Pro.local>
2022-03-03 20:27:45 -0500

    Modify services admin page table

--- webapp/src/page/admin/ServicesAdminPage.js
+++ webapp/src/page/admin/ServicesAdminPage.js
@@ -1,8 +1,10 @@
 import CustomMaterialTable from "../../component/CustomMaterialTable";
 import {QueryKeys} from "../../service/QueryKeys";
-import {useRef} from "react";
+import React, {useRef} from "react";
 import {TextFieldTableCell} from "../../component/TableCells";
 import ServiceService from "../../service/ServiceService";
+import {formatCurrency, getCurrency} from "../../utils/Utils";
+import {InputAdornment} from "@material-ui/core";
 
 const serviceService = new ServiceService();
 
@@ -11,10 +13,39 @@ export default function ServicesAdminPage({}) {
     const errorRef = useRef();
 
     const columns = [
-        {title: 'Name', field: 'name', editComponent: props => TextFieldTableCell(props, errorRef)},
-        {title: 'Duration (Minutes)', field: 'durationInMinutes', editComponent: props => TextFieldTableCell(props, errorRef), initialEditValue: 0},
-        {title: 'Price', field: 'price', editComponent: props => TextFieldTableCell(props, errorRef), initialEditValue: 0.00},
-        {title: 'Enabled', type: 'boolean', field: 'enabled', initialEditValue: true}
+        {
+            title: 'Name',
+            field: 'name',
+            editComponent: props => TextFieldTableCell(props, errorRef)
+        },
+        {
+            title: 'Duration (Minutes)',
+            field: 'durationInMinutes',
+            render: rowData => `${rowData.durationInMinutes} min.`,
+            editComponent: props => TextFieldTableCell(props, errorRef, "number", {
+                InputProps: {
+                    endAdornment: <InputAdornment position="end">min.</InputAdornment>
+                }
+            }),
+            initialEditValue: 0
+        },
+        {
+            title: 'Price',
+            field: 'price',
+            render: rowData => formatCurrency(rowData.price),
+            editComponent: props => TextFieldTableCell(props, errorRef, "number", {
+                InputProps: {
+                    startAdornment: <InputAdornment position="start">{getCurrency()}</InputAdornment>
+                }
+            }),
+            initialEditValue: 0.00
+        },
+        {
+            title: 'Active',
+            type: 'boolean',
+            field: 'enabled',
+            initialEditValue: true
+        }
     ];
 
     return (
